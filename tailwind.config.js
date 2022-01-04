758ee5b5b6c73811cd7447cb1089b9cb89541ad5
santiagoep <santiagoemp01@gmail.com>
2022-01-04 16:40:13 -0500

    Fixing tailwind config and adding fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages-components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      heading: ['eurostile', 'sans-serif'],
+      text: ['europa', 'sans-serif'],
+    },
   },
   plugins: [],
 };
