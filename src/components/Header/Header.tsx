b305299865c47f121ee72e6f9aaf50db296006e8
santiagoep <santiagoemp01@gmail.com>
2022-01-04 16:42:30 -0500

    Styling Header responsive and adding href to Say Hello Anchor

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,15 @@
 export const Header = () => {
   return (
-    <header className="flex justify-between items-center w-full md:max-w-5xl mx-auto p-5">
-      <div className="text-lg font-bold text-blue-500">@SantiagoEP</div>
-      <a className="border-2 border-blue-500 py-2 px-4 rounded-full text-blue-500 cursor-pointer hover:bg-blue-500 hover:text-white active:scale-105">
+    <header className="custom-general-container flex justify-between items-center py-4 md:py-5">
+      <div className="text-base md:text-lg font-bold text-blue-500 font-heading">
+        @SantiagoEP
+      </div>
+      <a
+        href="https://wa.me/573508108761"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-sm md:text-base border-2 border-blue-500 py-1 md:py-2 px-3 md:px-4 rounded-full text-blue-500 cursor-pointer hover:bg-blue-500 hover:text-white active:scale-105"
+      >
         Say Hello
       </a>
     </header>
