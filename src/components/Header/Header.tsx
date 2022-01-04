25f7f61a56f70232cd6629d7372f01c69fcc4a84
santiagoep <santiagoemp01@gmail.com>
2022-01-04 14:31:37 -0500

    Fixing Header HTML Semantic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
 export const Header = () => {
   return (
-    <nav className="flex justify-between items-center w-full md:max-w-5xl mx-auto p-5">
+    <header className="flex justify-between items-center w-full md:max-w-5xl mx-auto p-5">
       <div className="text-lg font-bold text-blue-500">@SantiagoEP</div>
       <a className="border-2 border-blue-500 py-2 px-4 rounded-full text-blue-500 cursor-pointer hover:bg-blue-500 hover:text-white active:scale-105">
         Say Hello
       </a>
-    </nav>
+    </header>
   );
 };
