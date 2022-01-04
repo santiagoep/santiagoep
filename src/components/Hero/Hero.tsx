74f9a0dbd70ff0560d6ac700932c66a5424b22ab
santiagoep <santiagoemp01@gmail.com>
2022-01-04 16:41:51 -0500

    Creating Hero

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+export const Hero = () => {
+  return (
+    <section className="custom-general-container text-center flex flex-col items-center md:py-24">
+      <h1 className="font-extrabold text-2xl md:text-5xl mt-6">
+        Fullstack Web Developer & Marketing Lover
+      </h1>
+      <p className="text-sm mx-10 md:mx-0 mt-4 md:text-2xl font-extralight leading-loose">
+        I code amazing software solutions, and I love what I do.
+      </p>
+      <div className="relative mt-6 md:mt-10 w-[118.2px] h-[152.1px] md:w-[197px] md:h-[253.6px]">
+        <Image
+          src="/images/me.png"
+          alt="Me @SantiagoEP"
+          layout="fill"
+          className="rounded-full object-fill"
+        />
+      </div>
+    </section>
+  );
+};
--- src/pages-components/Home/Home.tsx
+++ src/pages-components/Home/Home.tsx
@@ -1,9 +1,11 @@
+import { Hero } from 'components/Hero';
 import { Header } from 'components/Header';
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 };
