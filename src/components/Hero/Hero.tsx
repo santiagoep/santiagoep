dc96da286f59c3ea770d28f59c172dcf44cdc928
santiagoep <santiagoemp01@gmail.com>
2022-01-06 05:22:02 -0500

    Fixing Hero Styles

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 export const Hero = () => {
   return (
     <section className="custom-general-container text-center flex flex-col items-center md:py-24">
-      <h1 className="font-extrabold text-2xl md:text-5xl mt-6">
+      <h1 className="custom-main-gradient-text font-extrabold text-2xl md:text-5xl mt-6 pb-1">
         Fullstack Web Developer & Marketing Lover
       </h1>
-      <p className="text-sm font-extralight mx-10 md:mx-0 mt-4 md:mt-6 md:text-2xl">
+      <p className="text-sm font-extralight mx-10 md:mx-0 mt-3 md:mt-6 md:text-2xl">
         I code amazing software solutions and I love what I do.
       </p>
       <div className="relative mt-6 md:mt-10 w-[118.2px] h-[152.1px] md:w-[197px] md:h-[253.6px]">
