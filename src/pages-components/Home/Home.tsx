a1eb4efdd32bf81199f69b18af4975cb8f055e90
santiagoep <santiagoemp01@gmail.com>
2022-01-06 05:22:43 -0500

    Adding Services section to Home

--- src/pages-components/Home/Home.tsx
+++ src/pages-components/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { Hero } from 'components/Hero';
 import { Header } from 'components/Header';
+import { Services } from 'components/Services';
 import { CoverLetter } from 'components/CoverLetter';
 
 export const Home = () => {
@@ -8,6 +9,10 @@ export const Home = () => {
       <Header />
       <Hero />
       <CoverLetter />
+      <div className="bg-blue-500 w-full h-28" />
+      <section className="custom-general-container mt-[-112px]">
+        <Services />
+      </section>
     </>
   );
 };
