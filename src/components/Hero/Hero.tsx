e4b4efdaa2573b99212b789ee5bd1625c09a5a76
santiagoep <santiagoemp01@gmail.com>
2022-01-06 01:18:23 -0500

    Changing copies

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
         Fullstack Web Developer & Marketing Lover
       </h1>
       <p className="text-sm font-extralight mx-10 md:mx-0 mt-4 md:mt-6 md:text-2xl">
-        I code amazing software solutions, and I love what I do.
+        I code amazing software solutions and I love what I do.
       </p>
       <div className="relative mt-6 md:mt-10 w-[118.2px] h-[152.1px] md:w-[197px] md:h-[253.6px]">
         <Image
