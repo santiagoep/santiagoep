7fb1c3fdb730495103b82c4446a41dffd54936c2
santiagoep <santiagoemp01@gmail.com>
2022-02-14 22:05:05 -0500

    Refactoring Card @ui component

--- src/components/@ui/Card/Card.tsx
+++ src/components/@ui/Card/Card.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 export const Card: FC<CardProps> = ({ children, className }) => (
-  <div className={`shadow-md rounded-xl ${className}`}>{children}</div>
+  <article className={`shadow-md rounded-xl ${className}`}>{children}</article>
 );
 
 export interface CardProps {
