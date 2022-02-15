580959c4d4cdc6465d1cd4468224d76ebf93a85e
santiagoep <santiagoemp01@gmail.com>
2022-02-14 21:58:24 -0500

    Refactoring Header @layout component

--- src/components/@layout/Header/Header.tsx
+++ src/components/@layout/Header/Header.tsx
@@ -1,13 +1,19 @@
-export const Header = () => {
+import React from 'react';
+
+import { useContactInfo } from 'hooks/useContactInfo';
+
+export const Header: React.FC = () => {
+  const { findMe } = useContactInfo();
+
   return (
-    <header className="custom-general-container flex justify-between items-center py-4 md:py-5">
+    <header className="c_general-container flex justify-between items-center py-4 md:py-5">
       <p className="text-base md:text-lg font-bold text-blue-500 font-heading cursor-default">
         @SantiagoEP
       </p>
       <a
-        href="https://wa.me/573508108761"
+        href={findMe?.url}
         target="_blank"
-        rel="noopener noreferrer"
+        rel="noopener noreferrer nofollow"
         className="text-sm md:text-base border-2 border-blue-500 py-1 md:py-2 px-3 md:px-4 rounded-full text-blue-500 cursor-pointer hover:bg-blue-500 hover:text-white active:scale-105"
       >
         Find me
